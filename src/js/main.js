1c33cd96b509531ea80a92e0c7e6f295b8d8b54b
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2020-11-23 15:11:38 +0000

    Added a warning for when the device is too small

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 (function () {
-
+  resize()
+  window.addEventListener('resize', resize)
 }())
+
+function resize () {
+  window.innerWidth < 991.98 ? showWarning() : hideWarning()
+}
+
+function showWarning () {
+  $('.navbar, .footer, .top-section, .bottom-section').attr('style', 'display: none !important')
+  $('.warning').attr('style', 'display: block')
+  $('body').addClass('no-vis')
+}
+
+function hideWarning () {
+  $('.navbar, .top-section, .bottom-section').attr('style', 'display: block !important')
+  $('.footer').attr('style', 'display: flex !important')
+
+  $('.warning').attr('style', 'display: none')
+  $('body').removeClass('no-vis')
+}
