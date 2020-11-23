f76126f85c456eda6de71f117c62a26d917189f2
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2020-11-23 22:46:48 +0000

    Fixed map zoom and pan

--- src/js/map.js
+++ src/js/map.js
@@ -1,53 +1,38 @@
-(async function () {
-  console.log('Drawing map...')
+console.log('Drawing map...')
 
-  const width = '424'
-  const height = '224.4'
+const width = '100%'
+const height = '100%'
 
-  // Create the container SVG
-  const parent = d3.select('.map')
+const svg = d3.select('.map')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
 
-  // Remove whatever map with the same id/class was present before
-  parent.select('svg').remove()
+const projection = d3.geoMercator()
+  .scale(140)
+  .translate([210, 200])
+const path = d3.geoPath(projection)
 
-  // Initiate the map SVG
-  const svg = parent.append('svg')
-    .attr('width', width)
-    .attr('height', height)
+const g = svg.append('g')
 
-  // Create map
-  d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(topology => {
-    const projection = d3.geoMercator()
-      .translate([width / 2, height / 2])
-      .scale((width - 1) / 2 / Math.PI)
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(data => {
+  const countries = topojson.feature(data, data.objects.countries)
 
-    const path = d3.geoPath().projection(projection)
+  g.selectAll('path')
+    .data(countries.features)
+    .enter()
+    .append('path')
+    .attr('cl', 'country')
+    .attr('d', path)
+})
 
-    const zoom = d3.zoom()
-      .extent([
-        [0, 0],
-        [360, 224.4]
-      ])
-      .scaleExtent([1, 8])
-      .on('zoom', zoomed)
-
-    svg.selectAll('path')
-      .data(topojson.feature(topology, topology.objects.countries).features)
-      .enter()
-      .append('path')
-      .attr('d', path)
-      .style('fill', '#8484A0')
-      .attr('id', (datum, index) => {
-        return datum.properties.name
-      })
-      .call(zoom)
-
-    function zoomed ({ transform }) {
-      d3.select('.map')
-        .selectAll('path')
-        .attr('transform', transform)
-    }
-
-    console.log('Map - DONE!')
+const zoom = d3.zoom()
+  .scaleExtent([1, 8])
+  .on('zoom', ({ transform }) => {
+    g.selectAll('path')
+      .attr('transform', transform)
   })
-}())
+
+svg.call(zoom)
+
+console.log('Map - DONE!')
