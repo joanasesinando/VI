578aaea7d21849bb79bf86f1fc9b55f68ae3acec
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2020-11-25 17:05:59 +0000

    Fixed problems

--- src/js/map.js
+++ src/js/map.js
@@ -1,48 +1,50 @@
-console.log('Drawing map...')
+(function () {
+  console.log('Drawing map...')
 
-const width = '100%'
-const height = '100%'
+  const width = '100%'
+  const height = '100%'
 
-const svg = d3.select('.map')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
+  const svg = d3.select('.map')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
 
-const projection = d3.geoMercator()
-  .scale(140)
-  .translate([210, 200])
-const path = d3.geoPath(projection)
+  const projection = d3.geoMercator()
+    .scale(140)
+    .translate([210, 200])
+  const path = d3.geoPath(projection)
 
-const g = svg.append('g')
+  const g = svg.append('g')
 
-d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(data => {
-  const countries = topojson.feature(data, data.objects.countries)
+  d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(data => {
+    const countries = topojson.feature(data, data.objects.countries)
 
-  g.selectAll('path')
-    .data(countries.features)
-    .enter()
-    .append('path')
-    .attr('class', 'country')
-    .attr('d', path)
-    .append('title')
-    .text(d => d.properties.name)
-})
-
-const zoom = d3.zoom()
-  .scaleExtent([1, 8])
-  .on('zoom', ({ transform }) => {
     g.selectAll('path')
-      .attr('transform', transform)
+      .data(countries.features)
+      .enter()
+      .append('path')
+      .attr('class', 'country')
+      .attr('d', path)
+      .append('title')
+      .text(d => d.properties.name)
   })
 
-svg.call(zoom)
+  const zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on('zoom', ({ transform }) => {
+      g.selectAll('path')
+        .attr('transform', transform)
+    })
 
-d3.select('.map .zoom .plus').on('click', () => {
-  zoom.scaleBy(svg.transition().duration(200), 1.5)
-})
+  svg.call(zoom)
 
-d3.select('.map .zoom .minus').on('click', () => {
-  zoom.scaleBy(svg.transition().duration(200), 0.5)
-})
+  d3.select('.map .zoom .plus').on('click', () => {
+    zoom.scaleBy(svg.transition().duration(200), 1.5)
+  })
 
-console.log('Map - DONE!')
+  d3.select('.map .zoom .minus').on('click', () => {
+    zoom.scaleBy(svg.transition().duration(200), 0.5)
+  })
+
+  console.log('Map - DONE!')
+}())
